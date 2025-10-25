32daa08de1c8af05df8906d165e7242c6bd54b57
maxLiu <neoliukai@gmail.com>
2025-10-25 23:14:51 +0800

    feat: add validation functions for age and username fields

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,16 @@ import { FormsModule } from "@angular/forms";
 import { UltimateForm } from "./ultimate-form/ultimate-form";
 import { FieldConfig } from "./types";
 
+const isNotEmpty = (value: string) => value && value.trim().length > 0;
+const isTwoCharsOrMore = (value: string) => isNotEmpty(value) && value.trim().length >= 2;
+const is21OrOlder = (value: string) => {
+	const age = Number(value);
+	return !isNaN(age) && age >= 21;
+}
+const isCharsOrMore = (minChars: number) => {
+	return (value: string) => isNotEmpty(value) && value.trim().length >= minChars;
+};
+
 @Component({
 	selector: 'app-root',
 	imports: [ FormsModule, UltimateForm ],
@@ -12,7 +22,8 @@ import { FieldConfig } from "./types";
 export class App {
 	formFields: (FieldConfig | string)[] = [
 		'name',
-		{ name: 'age', type: 'number' },
+		{ name: 'age', type: 'number', validationFns: [ is21OrOlder ] },
+		{ name: 'username', validationFns: [ isTwoCharsOrMore ] },
 		{ name: 'email' },
 		{ name: 'password', displayName: 'Password', type: 'password', hint: 'Must be at least 8 characters long.' },
 		{ name: 'myFavoriteColor', displayName: 'My Favorite Color' },
--- src/app/types.ts
+++ src/app/types.ts
@@ -3,4 +3,5 @@ export interface FieldConfig {
 	displayName?: string;
 	type?: string;
 	hint?: string;
+	validationFns?: Function[];
 }
