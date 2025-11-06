98d4da846b82fdd2f4ee2b65f762042ddcc6b552
maxLiu <neoliukai@gmail.com>
2025-11-07 00:25:46 +0800

    feat: add number, password, and text input components with basic templates and tests

---
+++ src/app/number-input/number-input.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-number-input',
+  imports: [],
+  templateUrl: './number-input.html',
+  styleUrl: './number-input.css',
+})
+export class NumberInput {
+
+}
---
+++ src/app/password-input/password-input.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-input',
+  imports: [],
+  templateUrl: './password-input.html',
+  styleUrl: './password-input.css',
+})
+export class PasswordInput {
+
+}
