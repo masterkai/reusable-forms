a36e1bf5fdc80bf851c84a7e5f7868b71a33af07
max liu <max.liu@digitimes.com>
2026-04-16 13:54:39 +0800

    feat: make button visible and change validation trigger to submit

--- src/app/app.ts
+++ src/app/app.ts
@@ -71,21 +71,6 @@ export class App {
 			validators: [isEmailValidator]
 		},
 		{
-			name: 'birthday',
-			type: 'date',
-			hint: 'Format: YYYY-MM-DD',
-			validators: [createRegexValidator(datePattern, 'Date must be in YYYY-MM-DD format.')]
-		},
-		{
-			name: 'favoriteTimeOfDay',
-			displayName: 'Favorite Time of Day',
-			type: 'time',
-			hint: 'Format: HH:MM',
-			validators: [
-				createRegexValidator(timePattern, 'Time must be in HH:MM format.')
-			]
-		},
-		{
 			name: 'password',
 			displayName: 'Password',
 			type: 'password',
